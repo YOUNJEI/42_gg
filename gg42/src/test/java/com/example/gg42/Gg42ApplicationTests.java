fe7dec69f55f99e92d488b6747bb5dedd61b079a
YOUNJEI <kyj951211@gmail.com>
2022-06-28 18:51:58 +0900

    update : hello controller and test code

--- gg42/src/test/java/com/example/gg42/Gg42ApplicationTests.java
+++ gg42/src/test/java/com/example/gg42/Gg42ApplicationTests.java
@@ -2,7 +2,9 @@ package com.example.gg42;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:/application.properties")
 @SpringBootTest
 class Gg42ApplicationTests {
 
---
+++ gg42/src/test/java/com/example/gg42/web/HelloControllerTest.java
@@ -0,0 +1,44 @@
+package com.example.gg42.web;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerTest {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Value("${MYURL}")
+    private String MYURL;
+
+    @AfterEach
+    public void reset() throws Exception {
+
+    }
+
+    @Test
+    public void Hello_리턴된다() throws Exception {
+        // given
+        String expectedContent = "hello";
+        String URL = MYURL + port + "/";
+
+        // when
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(URL, String.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isEqualTo(expectedContent);
+    }
+}
