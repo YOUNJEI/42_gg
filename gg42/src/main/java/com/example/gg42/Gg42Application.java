932cff3d33796b91d9511b2709abc60f10f774f0
YOUNJEI <kyj951211@gmail.com>
2022-06-28 18:01:36 +0900

    init : 프로젝트 생성

---
+++ gg42/src/main/java/com/example/gg42/Gg42Application.java
@@ -0,0 +1,13 @@
+package com.example.gg42;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gg42Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gg42Application.class, args);
+    }
+
+}
