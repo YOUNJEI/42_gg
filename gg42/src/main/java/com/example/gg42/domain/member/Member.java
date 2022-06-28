67a75a0fbce87067fccf0aefafd093fd77cab34b
YOUNJEI <kyj951211@gmail.com>
2022-06-28 20:18:33 +0900

    update : MariaDB connection with JPA, and schema of Member

---
+++ gg42/src/main/java/com/example/gg42/domain/member/Member.java
@@ -0,0 +1,39 @@
+package com.example.gg42.domain.member;
+
+import com.example.gg42.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Member extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /*
+    @Column
+    private LocalDateTime deletedDate;
+     */
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    // TODO: 이미지 처리 방식 (URL, or anything else)
+    private String picture;
+
+    /*
+    @Column(nullable = false)
+    private boolean blackhole;
+     */
+
+    @Builder
+    public Member(String name) {
+        this.name = name;
+    }
+}
