e87fe7b0fbaf2093e80634d6ae4bf4711039a543
YOUNJEI <kyj951211@gmail.com>
2022-06-28 21:07:23 +0900

    update : indexController - login page

--- gg42/src/main/java/com/example/gg42/web/IndexController.java
+++ gg42/src/main/java/com/example/gg42/web/IndexController.java
@@ -1,13 +1,35 @@
 package com.example.gg42.web;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class IndexController {
 
+    @Value("${42APIUID}")
+    private String apiUid;
+
+    @Value("${42APIREDIRECTURI}")
+    private String apiRedirectUri;
+
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        //RestTemplate restTemplate = new RestTemplate();
+        //String response = "";
+        String requestURL = "https://api.intra.42.fr/oauth/authorize?"
+                + "client_id=" + apiUid + "&"
+                + "redirect_uri=" + apiRedirectUri + "&"
+                + "response_type=" + "code";
+
+        //response = restTemplate.getForObject(requestURL, String.class);
+        return "redirect:" + requestURL;
     }
 }
